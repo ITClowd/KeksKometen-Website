3500df0f5e65c9a2023c7ec4090d59e3f8eb8fa0
moritz <skyblock@gmx.de>
2016-11-24 22:04:35 +0100

    Added Example text + test.png + image-box feature

--- index.php
+++ index.php
@@ -7,13 +7,10 @@
         <link rel="stylesheet" href="css/stylesheet_menu.css">
     </head>
     <body>
-        <li>
-            <a href="index.php">Home</a>
-        </li>
-
         <div class="content">
-
-
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            </p>
+            <div class="image-box"><img src="images/test.png">Dies ist ein wunderschönes bild und ich teste etwas</div>
         </div>
     </body>
 </html>

6a70e1d0203a575647ffa3d531d238e10b435081
moritz <skyblock@gmx.de>
2016-11-24 19:40:26 +0100

    Added Stylesheets and other head tags

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-        
+        <meta lang="de">
+        <meta name="author" content="KeksKometen">
+        <link rel="stylesheet" href="css/stylesheet_basic.css">
+        <link rel="stylesheet" href="css/stylesheet_menu.css">
     </head>
     <body>
         

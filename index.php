488dd60a81234f6cfb1eb671baa62041ad899534
DwacLP <nils222@outlook.com>
2016-11-25 17:17:11 +0100

    menu

--- index.php
+++ index.php
@@ -7,9 +7,11 @@
         <link rel="stylesheet" href="css/stylesheet_menu.css">
     </head>
     <body>
-        <li>
-            <a href="index.php">Home</a>
-        </li>
+        <ul class="menu cf">
+            <li>
+                <a href="index.php">Home</a>
+            </li>
+        </ul>
         <div class="content">
             <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
             </p>

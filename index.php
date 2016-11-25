03e6a159b3135108f36f3244cc10868e6d0b952b
moritz <skyblock@gmx.de>
2016-11-25 17:38:19 +0100

    Added Header and some shadows

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
         <meta name="author" content="KeksKometen">
         <link rel="stylesheet" href="css/stylesheet_basic.css">
         <link rel="stylesheet" href="css/stylesheet_menu.css">
+        <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet"> 
     </head>
     <body>
         <ul class="menu cf">
@@ -13,8 +14,7 @@
             </li>
         </ul>
         <div class="content">
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            </p>
+            <h1>Die KeksKometen</h1>
             <div class="image-box"><img src="images/test.png">Dies ist ein wunderschönes bild und ich teste etwas</div>
         </div>
     </body>

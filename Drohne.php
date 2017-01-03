5ec5fb3ab00c2e9474f01b5beba9b73dec852210
DwacLP <nils222@outlook.com>
2017-01-03 17:07:56 +0100

    drohne.php

---
+++ Drohne.php
@@ -0,0 +1,31 @@
+<?php header("Content-Type: text/html; Charset=utf-8");?>
+    <html>
+
+    <head>
+        <title>KeksKometen</title>
+        <meta lang="de">
+        <meta name="author" content="KeksKometen">
+        <link href="css/stylesheet_menu.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/stylesheet_basic.css" rel="stylesheet" type="text/css" media="all" />
+        <meta charset="UTF-8">
+        <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+    </head>
+
+    <body>
+        <ul class="menu cf">
+            <li>
+                <a href="">Home</a>
+            </li>
+            <li>
+                <a href="">Projects</a>
+                <ul class="submenu">
+                    <li><a href="Drohne/">Drohe</a></li>
+                </ul>
+            </li>
+        </ul>
+        <div class="footer">
+            © 2016 KeksKometen
+        </div>
+    </body>
+
+    </html>

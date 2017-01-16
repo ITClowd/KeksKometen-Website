685c0bf79317f8985e839e87a9447f5e6a9f90ed
DwacLP <nils222@outlook.com>
2017-01-16 17:56:06 +0100

    com

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php header("Content-Type: text/html; Charset=utf-8");?>
+    <html>
+
+    <head>
+        <title>KeksKometen</title>
+        <meta lang="de">
+        <meta name="author" content="KeksKometen">
+        <link href="css/stylesheet_menu.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/stylesheet_basic.css" rel="stylesheet" type="text/css" media="all" />
+        <meta charset="UTF-8">
+        <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+    </head>
+
+    <body>
+        <ul class="menu cf">
+            <li>
+                <a href="index.php">Home</a>
+            </li>
+            <li>
+                <a href="">Projects</a>
+                <ul class="submenu">
+                    <li><a href="Drohne.php">Drohe</a></li>
+                </ul>
+            </li>
+        </ul>
+        <div class="content">
+            <h1>Die KeksKometen</h1>
+            <div class="image-box"><img src="images/test.png">Dies ist ein wunderschönes bild und ich teste etwas</div>
+        </div>
+        <div class="footer">
+            © 2016 KeksKometen
+        </div>
+    </body>
+
+    </html>
